3ee69463be5dcc87e93c39aa16bec7b4e8ee5021
BrenoMeneses <111250814+BrenoMeneses@users.noreply.github.com>
2023-03-27 20:32:34 -0300

    Add files via upload

--- List.js
+++ List.js
@@ -1,18 +1,38 @@
 var text = window.document.getElementById("addList")
 var formToDO = window.document.getElementById("formToDo")
+var toDo = window.document.getElementById("toDo")
 
 
 formToDO.addEventListener("submit", (e) =>{
     e.preventDefault()
 
     var valorTexto = text.value
-
     
+    if(valorTexto){
+        ToDo(valorTexto)
+    }
 })
 
 
 // funcoes 
 
-const saveToDO = (texto) => {
-    
+const ToDo = (text) => {
+    const div = window.document.createElement("div")
+    div.classList.add("lista")
+    toDo.appendChild(div)
+
+    const p = window.document.createElement("p")
+    p.innerText = text
+    div.appendChild(p)
+
+    const div2 = window.document.createElement("div")
+    div.appendChild(div2)
+
+    const btn1 = window.document.createElement("button")
+    btn1.innerText = "feito"
+    div2.appendChild(btn1)
+
+    const btn2 = window.document.createElement("button")
+    btn2.innerText = "excluir"
+    div2.appendChild(btn2)
 }
