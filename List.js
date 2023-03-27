c5a5d575c32de9b23feb78c1a845566afc6b6767
BrenoMeneses <brenomeneses525@gmail.com>
2023-03-27 17:05:53 -0300

    a

---
+++ List.js
@@ -0,0 +1,18 @@
+var text = window.document.getElementById("addList")
+var formToDO = window.document.getElementById("formToDo")
+
+
+formToDO.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    var valorTexto = text.value
+
+    
+})
+
+
+// funcoes 
+
+const saveToDO = (texto) => {
+    
+}
